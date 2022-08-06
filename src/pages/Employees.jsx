8321ca02e95f002ba025450878aee82363724b7b
Dozer <whitedylan7@gmail.com>
2022-08-06 04:52:43 -0500

    init commit

---
+++ src/pages/Employees.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { GridComponent, Inject, ColumnsDirective, ColumnDirective, Search, Page } from '@syncfusion/ej2-react-grids';
+
+import { UsersData, UsersGrid } from '../data/dummy';
+import { Header } from '../components';
+
+const Users = () => {
+  const toolbarOptions = ['Search'];
+
+  const editing = { allowDeleting: true, allowEditing: true };
+
+  return (
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="Database" title="ProdUsers" />
+      <GridComponent
+        dataSource={UsersData}
+        width="auto"
+        allowPaging
+        allowSorting
+        pageSettings={{ pageCount: 5 }}
+        editSettings={editing}
+        toolbar={toolbarOptions}
+      >
+        <ColumnsDirective>
+          {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+          {UsersGrid.map((item, index) => <ColumnDirective key={index} {...item} />)}
+        </ColumnsDirective>
+        <Inject services={[Search, Page]} />
+
+      </GridComponent>
+    </div>
+  );
+};
+export default Users;
